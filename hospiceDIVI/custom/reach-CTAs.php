1ab46fe2382f79661cb1568e7dc1202f97c8228c
Linda "zig" Speight <linda@reachmaine.com>
2020-02-11 16:12:11 -0500

    modifying header to look as I'd like

--- hospiceDIVI/custom/reach-CTAs.php
+++ hospiceDIVI/custom/reach-CTAs.php
@@ -5,7 +5,7 @@
 // under content widget area.
 	if ( function_exists('register_sidebar') ){
 
-		 // alerts box at top of site
+
 		register_sidebar(array(
 			'name' => 'Under Content Widget Area',
 			'id' => 'reach-under-content',
@@ -15,6 +15,17 @@
   		'before_title' => '<h4 class="widgettitle">',
   		'after_title' => '</h4>',
 		));
+
+		register_sidebar(array(
+			'name' => 'Header right Widget Area',
+			'id' => 'hvhc-header-right-widget',
+			'description' => 'Widget in header on rignt',
+      'before_widget' => '<div id="%1$s" class="et_pb_widget %2$s">',
+  		'after_widget' => '</div> <!-- end .et_pb_widget -->',
+  		'before_title' => '<h4 class="widgettitle">',
+  		'after_title' => '</h4>',
+		));
+
     }
 // add the widget to after the content using hook
   add_action('et_after_main_content', 'spp_under_content_area');
