0e7c13981f68f8b1f29aab3c620cd35951017069
Linda "zig" Speight <linda@reachmaine.com>
2022-12-27 12:21:16 -0500

    hide return to wordpress editor
    buttons

--- hospiceDIVI/custom/divi.php
+++ hospiceDIVI/custom/divi.php
@@ -29,3 +29,40 @@ if ( ! function_exists( 'et_get_footer_credits' ) ) {
   	return et_get_safe_localization( sprintf( $credits_format, $footer_credits, 'div' ) );
   }
 }
+/**
+ *	This will hide the Divi "Project" post type.
+ *	Thanks to georgiee (https://gist.github.com/EngageWP/062edef103469b1177bc#gistcomment-1801080) for his improved solution.
+ */
+add_filter( 'et_project_posttype_args', 'mytheme_et_project_posttype_args', 10, 1 );
+function mytheme_et_project_posttype_args( $args ) {
+	return array_merge( $args, array(
+		'public'              => false,
+		'exclude_from_search' => false,
+		'publicly_queryable'  => false,
+		'show_in_nav_menus'   => false,
+		'show_ui'             => false
+	));
+}
+
+/*hide buttons for return to standard editor or default editor
+founder here... https://www.peeayecreative.com/how-to-hide-the-gutenberg-and-standard-editor-buttons-in-divi/*/
+
+/*hide the Return To Standard Editor button and adjust button left margin*/
+add_action('admin_head', 'pa_hide_standard_editor_button');
+function pa_hide_standard_editor_button() {
+
+  echo '<style>
+       .et-db #et-boc .et-l #et_pb_toggle_builder.et_pb_builder_is_used { display: none; }
+       .et-db #et-boc .et-l #et_pb_fb_cta { margin-left: 0; }
+    </style>';
+}
+/*hide the Return To Default Editor button nad hide the Use Default Editor button*/
+add_action('admin_head', 'pa_hide_default_editor_button');
+function pa_hide_default_editor_button() {
+
+  echo '<style>
+    .block-editor__container .editor-post-switch-to-gutenberg.components-button.is-default {display: none; }
+    .block-editor__container #et-switch-to-gutenberg, .block-editor__container #et-switch-to-gutenberg.components-button.is-default {   display: none; }
+  </style>';
+}
+
